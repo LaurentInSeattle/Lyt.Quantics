c64fb60cfc12272fd78d36359c22091d58d92a32
Laurent <ly.testud@outlook.com>
2024-10-12 10:26:46 -0700

    Computer toolbar and commanding

---
+++ Lyt.Quantics.Studio/Messaging/ToolbarCommandMessage.cs
@@ -0,0 +1,21 @@
+﻿namespace Lyt.Quantics.Studio.Messaging;
+
+public sealed class ToolbarCommandMessage(
+    ToolbarCommandMessage.ToolbarCommand command, object? commandParameter = null)
+{
+    public enum ToolbarCommand
+    {
+        AddQubit,
+        RemoveQubit, 
+        HideProbabilities, 
+        Reset,
+        Step,
+        Run,
+        Loop,
+        Exit,
+    }
+
+    public ToolbarCommand Command { get; private set; } = command;
+
+    public object? CommandParameter { get; private set; } = commandParameter;
+}
--- Lyt.Quantics.Studio/Workflow/Run/Computer/ComputerToolbarViewModel.cs
+++ Lyt.Quantics.Studio/Workflow/Run/Computer/ComputerToolbarViewModel.cs
@@ -1,5 +1,40 @@
 ﻿namespace Lyt.Quantics.Studio.Workflow.Run.Computer;
 
-public sealed class ComputerToolbarViewModel: Bindable<ComputerToolbarView>
+using static ToolbarCommandMessage;
+
+public sealed class ComputerToolbarViewModel : Bindable<ComputerToolbarView>
 {
+    private void OnAddQubit(object? _) => this.Publish(ToolbarCommand.AddQubit);
+
+    private void OnRemoveQubit(object? _) => this.Publish(ToolbarCommand.RemoveQubit);
+
+    private void OnReset(object? _) => this.Publish(ToolbarCommand.Reset);
+
+    private void OnStep(object? _) => this.Publish(ToolbarCommand.Step);
+
+    private void OnRun(object? _) => this.Publish(ToolbarCommand.Run);
+
+    private void Publish(ToolbarCommand command, object? parameter = null)
+        => this.Messenger.Publish(new ToolbarCommandMessage(command, parameter));
+
+    public ICommand AddQubitCommand { get => this.Get<ICommand>()!; set => this.Set(value); }
+
+    public ICommand RemoveQubitCommand { get => this.Get<ICommand>()!; set => this.Set(value); }
+
+    public ICommand ResetCommand { get => this.Get<ICommand>()!; set => this.Set(value); }
+
+    public ICommand StepCommand { get => this.Get<ICommand>()!; set => this.Set(value); }
+
+    public ICommand RunCommand { get => this.Get<ICommand>()!; set => this.Set(value); }
+
+    public bool HideProbabilities
+    {
+        get => this.Get<bool>();
+        set
+        {
+            this.Set(value);
+            this.Publish(ToolbarCommand.HideProbabilities, value);
+        }
+    }
+
 }
