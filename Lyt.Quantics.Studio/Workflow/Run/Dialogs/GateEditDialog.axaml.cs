7258e8cbe9effff17e22eb207952b19ac1faf4f5
Laurent <ly.testud@outlook.com>
2024-11-02 11:31:56 -0700

    Progress on Gate Edit dialog, slider style complete

--- Lyt.Quantics.Studio/Workflow/Run/Dialogs/GateEditDialog.axaml.cs
+++ Lyt.Quantics.Studio/Workflow/Run/Dialogs/GateEditDialog.axaml.cs
@@ -2,18 +2,5 @@ namespace Lyt.Quantics.Studio.Workflow.Run.Dialogs;
 
 public partial class GateEditDialog : UserControl
 {
-    public GateEditDialog()
-    {
-        this.InitializeComponent();
-        this.PointerPressed += this.OnPointerPressed;
-    }
-
-    private void OnPointerPressed(object? sender, PointerPressedEventArgs pointerPressedEventArgs)
-    {
-        Debug.WriteLine("Pressed");
-        if (this.DataContext is GateEditDialogModel model)
-        {
-            model.OnDismiss(null); 
-        } 
-    }
+    public GateEditDialog() => this.InitializeComponent();
 }
--- Lyt.Quantics.Studio/Workflow/Run/Dialogs/GateEditDialogModel.cs
+++ Lyt.Quantics.Studio/Workflow/Run/Dialogs/GateEditDialogModel.cs
@@ -1,16 +1,58 @@
-﻿namespace Lyt.Quantics.Studio.Workflow.Run.Dialogs;
+﻿using static Lyt.Quantics.Studio.Messaging.ToolbarCommandMessage;
+
+namespace Lyt.Quantics.Studio.Workflow.Run.Dialogs;
 
 public sealed class GateEditDialogModel : DialogBindable<GateEditDialog, GateViewModel> 
 {
-    private void OnAction(object? _)
+    public GateEditDialogModel()
+    {
+    }
+
+    private GateViewModel GateViewModel 
+        => base.parameters is GateViewModel gVm ? 
+                gVm : 
+                throw new ArgumentNullException("No parameters"); 
+
+    protected override void OnViewLoaded()
+    {
+        base.OnViewLoaded();
+        this.ValuesCount = 10;
+        this.Title =
+            this.GateViewModel.Gate.CaptionKey.StartsWith('R') ?
+                "Gate Rotation Angle" :
+                "Gate Phase Value"; 
+    } 
+
+    private void OnSave(object? _)
     {
         this.onClose?.Invoke(this, true);
         this.dialogService.Dismiss();
     }
 
-    public void OnDismiss(object? _)
+    public void OnCancel(object? _)
     {
         this.onClose?.Invoke(this, false);
         this.dialogService.Dismiss();
     }
+
+    public string? Title { get => this.Get<string?>(); set => this.Set(value); }
+
+    public ICommand SaveCommand { get => this.Get<ICommand>()!; set => this.Set(value); }
+
+    public ICommand CancelCommand { get => this.Get<ICommand>()!; set => this.Set(value); }
+
+    public double ValuesCount { get => this.Get<double>(); set => this.Set(value); }
+
+    public double SliderValue
+    {
+        get => this.Get<double>();
+        set
+        {
+            this.Set(value);
+            int sliderValue = (int)Math.Round(this.SliderValue);
+            this.AngleValueText = string.Format("Angle: {0:D}", sliderValue);
+        }
+    }
+
+    public string AngleValueText { get => this.Get<string>()!; set => this.Set(value); }
 }
